ce598d60c66b6319d3435d2f179f71d57770252c
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2024-12-21 22:30:43 +0530

    added files

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header"
+import Moviecontainer from "../MovieContainer/Moviecontainer";
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Moviecontainer />
+        </>
+    )
+}
+
+export default Home;
